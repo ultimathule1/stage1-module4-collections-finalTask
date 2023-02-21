d0d1848c393a0c1c8fd2f9032bf09faa2c635831
madara <ranel28021998@gmail.com>
2023-02-21 17:03:18 +0300

    To be continued!

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -6,6 +6,10 @@ import java.util.Set;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        for (Map.Entry<String, Set<String>> map : projects.entrySet()) {
+            if (map.getKey() )
+        }
+
         return null;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -29,11 +29,19 @@ public class MapFromKeysCreator {
         for (Map.Entry<String, Integer> map : sourceMap.entrySet()) {
             length = map.getKey().length();
             value = map.getKey();
+            if (mainMap.containsKey(length)) {
+                continue;
+            }
             Set<String> tempSet = new LinkedHashSet<>();
             tempSet.add(value);
+            //sourceMap.remove(value);
+            for (Map.Entry<String, Integer> tempmap : sourceMap.entrySet()) {
+                if (tempmap.getKey().length() == length) {
+                    tempSet.add(tempmap.getKey());
+                }
+            }
             mainMap.put(length, tempSet);
         }
-
         return mainMap;
     }
 }
