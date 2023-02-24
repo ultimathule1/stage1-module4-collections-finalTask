21f7d7621905dc23dd3557084f6c39945069f170
Madara <ranel28021998@gmail.com>
2023-02-24 20:42:31 +0300

    done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,13 +1,10 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
-        List<String> list = new ArrayList<>();
+        List<String> list = new LinkedList<>();
         for (Map.Entry<String, Set<String>> map : projects.entrySet()) {
             Set<String> set = map.getValue();
             if (set.contains(developer)) {
